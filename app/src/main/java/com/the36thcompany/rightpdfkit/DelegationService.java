a45dfa46a5af9a12bdb894212ec5a33d901b72ba
mcdavies4 <azubuikedavies@gmail.com>
2026-05-28 21:43:24 +0100

    Add manifest and PWA icons

---
+++ app/src/main/java/com/the36thcompany/rightpdfkit/DelegationService.java
@@ -0,0 +1,14 @@
+package com.the36thcompany.rightpdfkit;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
